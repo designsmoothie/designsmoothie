f1076dc1bf218292c015ddbfc11b4a427e7847db
Design Smoothie <hello_smoohtie@naver.com>
2026-07-13 19:03:24 +0900

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,12 +128,23 @@ export default function RootLayout({
 />
 
 <Script id="google-analytics" strategy="afterInteractive">
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xlqykfucvc");
+</script>
+
   {`
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-5BWXM253QV');
+    gtag('config', 'G-5BWXM253QV', {
+  page_path: window.location.pathname,
+});
   `}
 </Script>
       </body>
